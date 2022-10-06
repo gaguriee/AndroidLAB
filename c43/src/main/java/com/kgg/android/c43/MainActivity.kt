7925b9b5f775d090d40be0dadd56cd8c086095b7
newkgg112 <newkgg112@gmail.com>
2022-10-06 19:06:54 +0900

    Implicit Intent

---
+++ c43/src/main/java/com/kgg/android/c43/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.kgg.android.c43
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent()
+            intent.action = "ACTION_DETAIL"
+            intent.data = Uri.parse("http://www.google.com")
+            startActivity(intent)
+        }
+    }
+}
