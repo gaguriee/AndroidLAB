ee06aeacfd00f99a2a79adb60707ab9f9228ff2a
newkgg112 <newkgg112@gmail.com>
2022-11-03 02:42:14 +0900

    Settings XML

---
+++ c56/src/main/java/com/kgg/android/c56/SettingsFragment.kt
@@ -0,0 +1,15 @@
+package com.kgg.android.c56
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
+
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+}
