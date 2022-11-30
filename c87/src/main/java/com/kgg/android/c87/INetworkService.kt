c7426c1bfd6c863395ef4e27f6f81058bf37b7f7
newkgg112 <newkgg112@gmail.com>
2022-11-30 17:41:58 +0900

    Retrofit 활용

---
+++ c87/src/main/java/com/kgg/android/c87/INetworkService.kt
@@ -0,0 +1,10 @@
+package com.kgg.android.c87
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface INetworkService {
+    @GET("api/users")
+    fun doGetUserList(@Query("page") page: String): retrofit2.Call<UserListModel>
+}
