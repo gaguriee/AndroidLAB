d7f1f17b6594e857f14facf2638c1d2056716015
newkgg112 <newkgg112@gmail.com>
2022-12-22 17:25:26 +0900

    DrawerLayout

---
+++ c96/src/main/java/com/kgg/android/c96/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.kgg.android.c96
+
+import android.os.Bundle
+import android.system.Os.close
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import com.kgg.android.c96.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.open, R.string.close)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
