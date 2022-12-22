46f2a1096195c5ad141d90ec8b4017c4d24aafac
newkgg112 <newkgg112@gmail.com>
2022-12-22 17:20:53 +0900

    FloatingActionButton

---
+++ c95/src/main/java/com/kgg/android/c95/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.kgg.android.c95
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kgg.android.c95.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener {
+            when(binding.fab.isExtended) {
+                true -> binding.fab.shrink()
+                false -> binding.fab.extend()
+            }
+        }
+    }
+}
